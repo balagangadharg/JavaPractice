c08375a02aa4d1a5d9bbae976157d52ccbaeee25
Gangadhara_g <Gangadhara_g@XOR-BSA-39.India.XoriantCorp.com>
2022-11-07 23:26:14 +0530

    Arthematic Operators is added.

---
+++ JavaPractice/src/Coding/Arthematic_Operators.java
@@ -0,0 +1,28 @@
+package Coding;
+
+public class Arthematic_Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=120, b=70;
+		
+		System.out.println("a + b = "+(a+b));
+		System.out.println("a - b = "+(a-b));
+		System.out.println("a * b = "+(a*b));
+		System.out.println("a / b = "+(a/b));
+		
+		System.out.print("a + b = "+(a+b));
+		System.out.print("a - b = "+(a-b));
+		System.out.print("a * b = "+(a*b));
+		System.out.print("a / b = "+(a/b));
+		
+		System.out.printf("a + b = "+(a+b));
+		System.out.printf("a - b = "+(a-b));
+		System.out.printf("a * b = "+(a*b));
+		System.out.printf("a / b = "+(a/b));
+		
+
+	}
+
+}
